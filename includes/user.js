8e3837905b1b430e959294e4eb8efb4df4169be6
Matt Collins <matt@scoot.co.uk>
2012-10-28 19:50:20 +0000

    Alter create user API call to accept password and hostname, also limit each email address to 5 API keys and enforce consistent passwords

--- includes/user.js
+++ includes/user.js
@@ -255,11 +255,27 @@ var stats = function stats(callback) {
 
 var createApiUser = function(user, callback) {
   
-  if (typeof user.email == 'string' && typeof user.name == 'string') {
-    var newUser = {};
-    newUser.email = user.email;
-    newUser.name = user.name;
-    
+  var messages = [];
+  
+  if (typeof user.email != 'string' || !user.email) {
+    messages.push("You must supply an email address");
+  }
+  
+  if (typeof user.name != 'string' || !user.name) {
+    messages.push("You must supply a name");
+  }
+  
+  if (typeof user.password != 'string' || !user.password) {
+    messages.push("You must supply a password");
+  }
+  
+  if (typeof user.hostname != 'string' || !user.hostname) {
+    messages.push("You must supply a hostname");
+  }
+  
+  if (messages.length == 0) {
+        
+    // generate api key
     var d = new Date;
     var t = d.getTime();
     
@@ -267,13 +283,45 @@ var createApiUser = function(user, callback) {
     md5.update(t.toString());
     apikey = md5.digest('hex');
     
-    db.save(apikey, newUser, function() {
-      callback(null, {"apikey": apikey});
+    db.view('api/byEmail', {key: user.email, include_docs: true}, function(err, docs) {
+      
+      //if we have an error
+      if (err) {
+        callback(err, null);
+      }
+      
+      // no errors!
+      else {
+        
+        // do we have 5 docs for this email already?
+        if (docs.length == 5) {
+          callback(["Maximum number of API keys already registered"], null);
+        }
+        
+        // not hit maximum yet
+        else {
+          var warnings = [];
+          
+          // if we have existing docs, force password to be the same as those passwords
+          if (docs.length > 0) {
+            user.password = docs[0].doc.password;
+            warnings.push("Password changed to match previous entries for this email address");
+          }
+          
+          db.save(apikey, user, function() {
+            callback(null, {"apikey": apikey, "warnings": warnings});
+          });
+          
+        }
+        
+      }
+      
     });
+    
   }
   
   else {
-    callback(["You must supply both of the following parameters: email, name"], null);
+    callback(messages, null);
   }
 }
 
