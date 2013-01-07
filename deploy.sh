db620b97ae57a3a25bcaf57f6a83f12a07d39183
Matt Collins <matt@strikesandgutters.com>
2013-01-07 19:33:33 +0000

    change deply script

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ssh ubuntu@ec2-176-34-91-164.eu-west-1.compute.amazonaws.com "cd ~/insto; git pull; npm install; forever stop insto.js; forever start insto.js"
+ssh ubuntu@api.insto.co.uk "cd ~/insto; git pull; npm install; forever stop insto.js; forever start insto.js"
