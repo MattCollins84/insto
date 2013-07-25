4251f7be89e7393c518ef097c1dfb122e31cc44a
Matt Collins <matt@scoot.co.uk>
2013-07-25 11:38:48 +0100

    dont restart tweeter

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ssh ubuntu@api.insto.co.uk "cd ~/insto; git pull; npm install; forever stop insto.js; forever start insto.js --protocol https; cd ~/tweeter; forever stop tweeter.js; forever start tweeter.js"
+ssh ubuntu@api.insto.co.uk "cd ~/insto; git pull; npm install; forever stop insto.js; forever start insto.js --protocol https;"
